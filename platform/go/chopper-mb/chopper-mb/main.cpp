34ed10d170e5af0e5299d620f6e21bb52d47f3af
Wes Richardet <wes@tetriscodes.com>
2018-02-26 09:08:48 -0600

    Fixing build for osx

--- platform/go/chopper-mb/chopper-mb/main.cpp
+++ platform/go/chopper-mb/chopper-mb/main.cpp
@@ -13,21 +13,22 @@
 #include <mbgl/gl/headless_backend.hpp>
 #include <mbgl/util/shared_thread_pool.hpp>
 
-void doIt(mbgl::FileSource *fileSource) {
-  std::unique_ptr<mbgl::Map> mapFront;
+int doIt(mbgl::FileSource *fileSource) {
+    std::unique_ptr<mbgl::Map> mapFront;
   std::unique_ptr<mbgl::Map> mapBack;
   std::unique_ptr<mbgl::HeadlessFrontend> frontend;
   std::shared_ptr<mbgl::ThreadPool> scheduler = mbgl::sharedThreadPool();
-  frontend = std::make_unique<mbgl::HeadlessFrontend>(mbgl::Size{ 256, 256 },1.0f,fileSource,scheduler);
-  std::unique_ptr<mbgl::HeadlessBackend> backend;
-  backend = std::make_unique<mbgl::HeadlessBackend>(mbgl::Size{256, 256})
-  mapFront = std::make_unique<mbgl::Map>(*frontend,nullptr,frontend->getSize(),1.0f,fileSource,scheduler,mbgl::MapMode::Tile);
-  mapBack = std::make_unique<mbgl::Map>()
+  frontend = mbgl::HeadlessFrontend(mbgl::Size{ 256, 256 },1.0f,fileSource,scheduler);
+//  std::unique_ptr<mbgl::HeadlessBackend> backend;
+//    backend = std::make_unique<mbgl::HeadlessBackend>(mbgl::Size{256, 256});
+//  mapFront = std::make_unique<mbgl::Map>(*frontend,,frontend->getSize(),1.0f,fileSource,scheduler,mbgl::MapMode::Tile);
+//    mapFront->getStyle().
+    return 1;
 }
 
 int main(int argc, const char * argv[]) {
 
   // insert code here...
-  std::cout << "Hello, World!\n";
+  std::cout << "hello world";
   return 0;
 }
